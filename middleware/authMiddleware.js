64f349f826c9377c590aa2f2fbd59d26b5a79216
adrianoteruo <11221101871@alunos.umc.br>
2025-11-17 20:50:21 -0300

    Update authMiddleware.js

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,8 +1,5 @@
-// middleware/authMiddleware.js
 const jwt = require('jsonwebtoken');
 const JWT_SECRET = require('../config/jwt'); 
-
-//Middleware para verificar o Token JWT.
 
 const verifyToken = (req, res, next) => {
     const authHeader = req.headers['authorization'];
@@ -18,7 +15,6 @@ const verifyToken = (req, res, next) => {
             return res.status(403).json({ message: 'Token inválido ou expirado.' });
         }
         
-        // Anexa os dados do usuário ao request
         req.user = user; 
         next(); 
     });
