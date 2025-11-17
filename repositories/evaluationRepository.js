e0bf929cb31324ce932d56cd12326ecb3e90e21a
adrianoteruo <11221101871@alunos.umc.br>
2025-11-17 20:58:59 -0300

    Update evaluationRepository.js

--- repositories/evaluationRepository.js
+++ repositories/evaluationRepository.js
@@ -1,7 +1,5 @@
-// repositories/evaluationRepository.js
 const pool = require('../config/database');
 
-// Cria uma nova avaliação.
 const create = async (evaluatorId, evaluated_id, rating, comment) => {
     const sql = `
         INSERT INTO evaluations (evaluator_id, evaluated_id, rating, comment) 
@@ -10,7 +8,7 @@ const create = async (evaluatorId, evaluated_id, rating, comment) => {
     await pool.execute(sql, [evaluatorId, evaluated_id, rating, comment || '']);
 };
 
-// Busca todas as avaliações que um usuário recebeu.
+
 const findByEvaluatedId = async (evaluatedId) => {
     const sql = `
         SELECT e.rating, e.comment, e.created_at, u.nome_completo AS evaluator_name 
