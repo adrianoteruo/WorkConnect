d97386920a84238298d994fff19ba7689f0935e4
adrianoteruo <11221101871@alunos.umc.br>
2025-11-17 20:50:38 -0300

    Update roleMiddleware.js

--- middleware/roleMiddleware.js
+++ middleware/roleMiddleware.js
@@ -1,13 +1,10 @@
-// middleware/roleMiddleware.js
-
-//Middleware para verificar o perfil do usuário.
 const checkRole = (roles) => {
     return (req, res, next) => {
-        // req.user é anexado pelo middleware verifyToken
+
         const userRole = req.user.role; 
 
         if (userRole && roles.includes(userRole)) {
-            next(); // O usuário tem o perfil permitido
+            next(); 
         } else {
             res.status(403).json({ message: 'Acesso negado. Você não tem permissão para este recurso.' });
         }
