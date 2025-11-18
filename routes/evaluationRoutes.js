0cd476ccb92a557203514a585f285261261ea50a
adrianoteruo <11221101871@alunos.umc.br>
2025-11-17 21:02:13 -0300

    Update evaluationRoutes.js

--- routes/evaluationRoutes.js
+++ routes/evaluationRoutes.js
@@ -1,17 +1,16 @@
-// routes/evaluationRoutes.js
 const express = require('express');
 const router = express.Router();
 const evaluationController = require('../controllers/evaluationController');
 const { checkRole } = require('../middleware/roleMiddleware');
 
-// POST /api/evaluations
+
 router.post(
     '/evaluations', 
     checkRole(['Contratante', 'Profissional']), 
     evaluationController.createEvaluation
 );
 
-// GET /api/my-evaluations
+
 router.get(
     '/my-evaluations', 
     evaluationController.getMyEvaluations 
