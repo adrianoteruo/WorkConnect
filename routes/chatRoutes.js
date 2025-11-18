dd5b59a2f3bc0f4fe69698541c21697f12fdde91
adrianoteruo <11221101871@alunos.umc.br>
2025-11-17 21:01:38 -0300

    Update chatRoutes.js

--- routes/chatRoutes.js
+++ routes/chatRoutes.js
@@ -1,9 +1,8 @@
-// routes/chatRoutes.js
 const express = require('express');
 const router = express.Router();
 const chatController = require('../controllers/chatController');
 
-// GET /api/chat/:user1_id/:user2_id
+
 router.get(
     '/chat/:user1_id/:user2_id', 
     chatController.getChatHistory 
