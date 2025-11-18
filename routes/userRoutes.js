3a22cd792ed722d1ff7ebe9cbb34d85d94a8f375
adrianoteruo <11221101871@alunos.umc.br>
2025-11-17 21:03:21 -0300

    Update userRoutes.js

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,10 +1,8 @@
-// routes/userRoutes.js
 const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/userController');
 const { checkRole } = require('../middleware/roleMiddleware');
 
-// GET /api/users (Rota exclusiva do Admin)
 router.get(
     '/users', 
     checkRole(['Admin']), 
