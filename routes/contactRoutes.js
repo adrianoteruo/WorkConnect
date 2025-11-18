a6972698a65dcf367a41d5d6b4a938a304ce7340
adrianoteruo <11221101871@alunos.umc.br>
2025-11-17 21:01:56 -0300

    Update contactRoutes.js

--- routes/contactRoutes.js
+++ routes/contactRoutes.js
@@ -1,24 +1,23 @@
-// routes/contactRoutes.js
 const express = require('express');
 const router = express.Router();
 const contactController = require('../controllers/contactController');
 const { checkRole } = require('../middleware/roleMiddleware');
 
-// POST /api/contacts
+
 router.post(
     '/contacts', 
     checkRole(['Contratante']), 
     contactController.createContact
 );
 
-// GET /api/my-contacts
+
 router.get(
     '/my-contacts', 
     checkRole(['Profissional']), 
     contactController.getMyContacts
 );
 
-// GET /api/my-conversations
+
 router.get(
     '/my-conversations', 
     checkRole(['Contratante']), 
