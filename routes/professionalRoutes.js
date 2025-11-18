d02bb35c58944916ec2005ff6d5d3952931b1180
adrianoteruo <11221101871@alunos.umc.br>
2025-11-17 21:02:45 -0300

    Update professionalRoutes.js

--- routes/professionalRoutes.js
+++ routes/professionalRoutes.js
@@ -1,17 +1,15 @@
-// routes/professionalRoutes.js
 const express = require('express');
 const router = express.Router();
 const professionalController = require('../controllers/professionalController');
 const { checkRole } = require('../middleware/roleMiddleware');
 
-// GET /api/professionals
 router.get(
     '/professionals', 
     checkRole(['Contratante', 'Admin']), 
     professionalController.getProfessionals
 );
 
-// GET /api/professional-stats
+
 router.get(
     '/professional-stats',
     checkRole(['Profissional']),
