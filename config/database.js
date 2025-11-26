6f7e333872425eeb5e259c016b1a90d660cdd411
adrianoteruo <11221101871@alunos.umc.br>
2025-11-26 11:20:06 -0300

    Update database.js

--- config/database.js
+++ config/database.js
@@ -8,10 +8,10 @@ const dbConfig = {
     database: 'workconnect'
 };
 
-// Criar pool de conexões
+
 const pool = mysql.createPool(dbConfig);
 
-// Testar a conexão
+
 async function testConnection() {
     try {
         const connection = await pool.getConnection();
