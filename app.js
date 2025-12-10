bc1fad6be91b9a623b3b389a894a92f93f0c5ebc
adrianoteruo <11221101871@alunos.umc.br>
2025-12-10 12:35:08 -0300

    Update app.js

--- app.js
+++ app.js
@@ -21,14 +21,21 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 
 app.use((req, res, next) => {
-    const allowedOrigins = ['http://localhost:3000', 'http://127.0.0.1:3000'];
+
+    const allowedOrigins = process.env.ALLOWED_ORIGINS 
+        ? process.env.ALLOWED_ORIGINS.split(',') 
+        : [];
+
     const origin = req.headers.origin;
+
     if (allowedOrigins.includes(origin)) {
         res.header('Access-Control-Allow-Origin', origin);
     }
+
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization, user-role');
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
     res.header('Access-Control-Allow-Credentials', true);
+
     if (req.method === 'OPTIONS') {
         return res.sendStatus(200);
     }
